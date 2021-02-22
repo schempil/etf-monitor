5c1b7c14333071563fe2f50a5dd3d4a826429cea
Philipp Schemel <philippschemel@live.com>
2021-02-22 23:55:37 +0100

    Refactor App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import './App.css';
 import {HeaderBar} from "./headerBar/HeaderBar";
 import {EtfList} from "./etfList/EtfList";
+import {createUseStyles} from "react-jss";
 
 
 function App() {
 
+	const useStyles = createUseStyles({
+		AppHeader: {
+			backgroundColor: '#282c34',
+			minHeight: '100vh'
+		}
+	})
+
+	const classes = useStyles()
+
   return (
     <div className="App" data-testid="App Container">
-      <header className="App-header">
+      <header className={classes.AppHeader}>
 					<HeaderBar />
 					<EtfList />
       </header>
