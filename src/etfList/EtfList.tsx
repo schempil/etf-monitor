a93df9487f59319866f2c6eb868bf1b9471ab920
Philipp Schemel <philippschemel@live.com>
2021-02-23 13:17:05 +0100

    Refactor EtfList with state and lifecycle methods to function component with hooks

--- src/etfList/EtfList.tsx
+++ src/etfList/EtfList.tsx
@@ -1,59 +1,47 @@
-import React from "react";
 import {Etf} from "../types/Etf";
 import './EtfList.css';
-import {MockService} from "../services/MockService";
+import {useEffect, useState} from "react";
 import {EtfCard} from "../etfCard/EtfCard";
 import {NewEtf} from "../newEtf/NewEtf";
+import {MockService} from "../services/MockService";
 
-type State = {
-	etfList: Etf[]
-	activeEtf: Etf | null
-}
+export function EtfList() {
 
-export class EtfList extends React.Component<any, State> {
+	const [etfList, setEtfList] = useState<Etf[]>([])
+	const [activeEtf, setActiveEtf] = useState<Etf | null>(null)
 
-	constructor(props: any) {
-		super(props);
-
-		this.addEtf = this.addEtf.bind(this)
-		this.setActive = this.setActive.bind(this)
-
-		this.state = {
-			etfList: [],
-			activeEtf: null
-		}
-	}
-
-	async componentDidMount() {
-		this.setState({
-			etfList: await MockService.getEtfList()
+	useEffect(() => {
+		MockService.getEtfList().then(etfList => {
+			setEtfList(etfList)
 		})
+	}, [])
+
+	const addEtf = (etf: Etf) => {
+
+		//TODO Needs to clarify in here, very bad solution here...
+
+		const newList = JSON.parse(JSON.stringify(etfList))
+		newList.push(etf)
+		setEtfList(newList)
 	}
 
-	addEtf(etf: Etf) {
-		this.state.etfList.push(etf)
-		this.setState({ etfList: this.state.etfList } )
+	const setActive = (etf: Etf) => {
+		setActiveEtf(etf)
 	}
 
-	setActive(etf: Etf) {
-		this.setState({ activeEtf: etf } )
-	}
-
-	render() {
-
-		const listItems = this.state.etfList.map((etf, index) => {
-			return (
-				<EtfCard etf={etf} key={etf.name} activeEtf={this.state.activeEtf} setActive={this.setActive} />
-			)
-		})
-
+	const listItems = etfList.map((etf, index) => {
 		return (
-			<div className="container etf-list">
-				<NewEtf addEtf={this.addEtf} />
-				<div className="row row-cols-1 row-cols-md-3 g-4">
-					{listItems}
-				</div>
-			</div>
+			<EtfCard etf={etf} key={etf.name} activeEtf={activeEtf} setActive={setActive} />
 		)
-	}
+	})
+
+	return (
+		<div className="container etf-list">
+			<NewEtf addEtf={addEtf} />
+			<div className="row row-cols-1 row-cols-md-3 g-4">
+				{listItems}
+			</div>
+		</div>
+	)
+
 }
