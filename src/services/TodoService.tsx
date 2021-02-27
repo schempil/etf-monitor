d2f174f6f9468605804574b0dc29539e822af058
Philipp Schemel <philippschemel@live.com>
2021-02-27 12:48:27 +0100

    Filter todo list by user

--- src/services/TodoService.tsx
+++ src/services/TodoService.tsx
@@ -1,7 +1,7 @@
 export class TodoService {
 
 	static getTodos = async() => {
-		return await fetch("https://jsonplaceholder.typicode.com/todos").then(response => response.json())
+		return await fetch("https://jsonplaceholder.typicode.com/todos?userId=1").then(response => response.json())
 	}
 
 }
